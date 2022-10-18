c8e4d195bde380a3d8ae9d3f34762245345fdd15
adominguezfanlo <adominguez@vicomtech.org>
2022-10-18 11:13:05 +0200

    Update loadImageDockers.sh

--- backend/loadImageDockers.sh
+++ backend/loadImageDockers.sh
@@ -4,4 +4,4 @@ docker load < images/traction_motion.tar.gz
 docker load < images/traction_orkestraserver.tar.gz
 docker load < images/traction_reverseproxy.tar.gz
 docker load < images/traction_static.tar.gz 
-docker load < images/traction_endoningapi.tar.gz
+docker load < images/traction_encodingapi.tar.gz
