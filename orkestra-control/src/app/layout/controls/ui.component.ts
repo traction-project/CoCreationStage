f00f12c6dc81399a5a4445afeaf7d8b03d65cb61
adominguez <adominguez@vicomtech.org>
2022-10-19 16:02:40 +0200

    add license headings

--- orkestra-control/src/app/layout/controls/ui.component.ts
+++ orkestra-control/src/app/layout/controls/ui.component.ts
@@ -1,3 +1,8 @@
+/* << Copyright 2022 Tamayo Uria, Ana Domínguez Fanlo, Mikel Joseba Zorrilla Berasategui, Héctor Rivas Pagador, Sergio Cabrero Barros, Juan Felipe Mogollón Rodríguez, Daniel Mejías y Stefano Masneri >>
+This file is part of CoCreationStage.
+CoCreationStage is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+CoCreationStage is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with Orkestralib. If not, see <https://www.gnu.org/licenses/>. */
 import {  ChangeDetectorRef, Component, ElementRef } from '@angular/core';
 import {Orkestra,URI,UserTable,AppTable,KeyPress} from 'orkestraLib';
 import {TextData} from 'orkestraLib';
