2b0c2646af12734f2d6ae6b924e259265c653dac
Hector Rivas Pagador <hrivas@vicomtech.org>
2022-10-19 14:18:52 +0200

    Removed static and reverseproxy docker images

--- backend/loadImageDockers.sh
+++ backend/loadImageDockers.sh
@@ -2,6 +2,4 @@
 docker load < images/traction_janus.tar.gz
 docker load < images/traction_motion.tar.gz
 docker load < images/traction_orkestraserver.tar.gz
-docker load < images/traction_reverseproxy.tar.gz
-docker load < images/traction_static.tar.gz 
 docker load < images/traction_encodingapi.tar.gz
